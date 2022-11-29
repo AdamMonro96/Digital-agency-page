9768c647a6ac2b63dc84653b017a2871b1bdeaa0
Adam <yourmonitor@mail.ru>
2022-11-29 19:57:32 +0300

    Errors has been fixed

---
+++ js/form.js
@@ -0,0 +1,12 @@
+let form = document.querySelector(".contact-form");
+let buttom = form.querySelector(".contact-form__button");
+let inputs = form.querySelectorAll("input");
+
+console.log(inputs);
+
+// function formCheck(evt){
+//     for (input of inputs){
+//         if (input.type == "text")
+       
+//     }
+// }
